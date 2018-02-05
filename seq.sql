cd8de8bfb0e37f12a4156c03818a0af070bfe09e
Michal Sroka <m4jk3ls@gmail.com>
2018-02-05 17:51:51 +0100

    Question divided by QUESTION_CONTENT and QUESTION_ATTRIBUTES + correct sequences

--- seq.sql
+++ seq.sql
@@ -1,6 +1,14 @@
-DROP SEQUENCE question_seq;
+DROP SEQUENCE question_content_seq;
+DROP SEQUENCE question_attributes_seq;
 
-CREATE SEQUENCE question_seq
+---------------------------------------------
+
+CREATE SEQUENCE question_content_seq
+	MINVALUE 1
+	START WITH 1
+	INCREMENT BY 1;
+    
+CREATE SEQUENCE question_attributes_seq
 	MINVALUE 1
 	START WITH 1
 	INCREMENT BY 1;
--- tab.sql
+++ tab.sql
@@ -1,7 +1,8 @@
 /*First start PL/SQL block, then create tables*/
 
 BEGIN
-	EXECUTE IMMEDIATE 'DROP TABLE question CASCADE CONSTRAINTS';
+	EXECUTE IMMEDIATE 'DROP TABLE question_content CASCADE CONSTRAINTS';
+    EXECUTE IMMEDIATE 'DROP TABLE question_attributes CASCADE CONSTRAINTS';
 	EXECUTE IMMEDIATE 'DROP TABLE active_question CASCADE CONSTRAINTS';
 	EXECUTE IMMEDIATE 'DROP TABLE archives CASCADE CONSTRAINTS';
 EXCEPTION
@@ -13,13 +14,23 @@ END;
 
 -------------------------------------------------------------------------------------------------
 
-CREATE TABLE question (
+CREATE TABLE question_content (
 	id NUMBER(10) NOT NULL,
 	content VARCHAR2(255) NOT NULL,
+    	CONSTRAINT question_content_pk PRIMARY KEY (id),
+		CONSTRAINT question_content_unique UNIQUE (content)
+);
+
+CREATE TABLE question_attributes (
+	id NUMBER(10) NOT NULL,
+    question_content_id NUMBER(10) NOT NULL,
 	question_about VARCHAR2(50) NOT NULL,
 	question_context VARCHAR2(50) NOT NULL,
-    	CONSTRAINT question_pk PRIMARY KEY (id),
-		CONSTRAINT question_unique UNIQUE (content)
+	question_about_api VARCHAR2(50) NOT NULL,
+	question_context_api VARCHAR2(50) NOT NULL,
+    	CONSTRAINT question_attributes_pk PRIMARY KEY (id),
+        CONSTRAINT question_unique UNIQUE (question_content_id, question_about_api, question_context_api),
+        CONSTRAINT content_in_attributes_fk FOREIGN KEY (question_content_id) REFERENCES question_content(id) ON DELETE CASCADE
 );
 
 CREATE TABLE active_question (
@@ -27,7 +38,7 @@ CREATE TABLE active_question (
     question_id NUMBER(10) NOT NULL,
     answer VARCHAR2(1000) NOT NULL,
     	CONSTRAINT active_question_pk PRIMARY KEY (token),
-        CONSTRAINT question_in_active_question_fk FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE
+        CONSTRAINT question_in_active_question_fk FOREIGN KEY (question_id) REFERENCES question_attributes(id) ON DELETE CASCADE
 );
 
 CREATE TABLE archives (
@@ -35,5 +46,5 @@ CREATE TABLE archives (
     good_answers NUMBER(10) DEFAULT 0 NOT NULL,
     bad_answers NUMBER(10) DEFAULT 0 NOT NULL,
     	CONSTRAINT question_in_archives_unique UNIQUE (question_id),
-    	CONSTRAINT question_in_archives_fk FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE
+    	CONSTRAINT question_in_archives_fk FOREIGN KEY (question_id) REFERENCES question_attributes(id) ON DELETE CASCADE
 );
