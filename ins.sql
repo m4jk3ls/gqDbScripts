a0c52c5a6d5af62e5d812295eda0845c13973222
Michal Sroka <m4jk3ls@gmail.com>
2018-02-16 21:55:06 +0100

    Delete QUESTION_CONTEXT column

--- ins.sql
+++ ins.sql
@@ -6,48 +6,40 @@ DELETE FROM archives;
 -----------------------------------------------
 
 INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'What is the <?> of <?>?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'capital', 1, 'capital', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'capital', 1, 'capital');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'currency', 1, 'currencies|name', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'currency', 1, 'currencies|name');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'language', 1, 'languages|name', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'language', 1, 'languages|name');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'population', 1, 'population', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'population', 1, 'population');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'demonym', 1, 'demonym', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'demonym', 1, 'demonym');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'native name', 1, 'nativeName', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'native name', 1, 'nativeName');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'numeric code', 1, 'numericCode', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'numeric code', 1, 'numericCode');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'area', 1, 'area', 'name');
-INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
-
-INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'What is the name of the presented <?> of <?>?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'subregion', 1, 'subregion', 'region');
-INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
-
-INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'On which <?> is the presented <?>?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'continent', 1, 'region', 'subregion');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'area', 1, 'area');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
 INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'Which <?> does <?> belong to?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'regional block', 1, 'regionalBlocs|name', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'regional block', 1, 'regionalBlocs|name');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
 INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'Which of the listed <?> borders <?>?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'countries', 1, 'borders', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'countries', 1, 'borders');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
 INSERT INTO question_content VALUES (question_content_seq.NEXTVAL, 'Which of the listed <?> is related to <?>?');
-INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'time zones', 1, 'timezones', 'name');
+INSERT INTO question_attributes VALUES (question_attributes_seq.NEXTVAL, question_content_seq.CURRVAL, 'time zones', 1, 'timezones');
 INSERT INTO archives (id, question_attributes_id) VALUES (archives_seq.NEXTVAL, question_attributes_seq.CURRVAL);
 
 COMMIT;
--- tab.sql
+++ tab.sql
@@ -6,27 +6,26 @@ DROP TABLE archives CASCADE CONSTRAINTS;
 -------------------------------------------------------------------------------------------------
 
 CREATE TABLE question_content (
-	id NUMBER(10) NOT NULL,
+	id NUMBER NOT NULL,
 	content VARCHAR2(255) NOT NULL,
     	CONSTRAINT question_content_pk PRIMARY KEY (id),
 		CONSTRAINT question_content_unique UNIQUE (content)
 );
 
 CREATE TABLE question_attributes (
-	id NUMBER(10) NOT NULL,
-    question_content_id NUMBER(10) NOT NULL,
+	id NUMBER NOT NULL,
+    question_content_id NUMBER NOT NULL,
 	question_attribute VARCHAR2(50) NOT NULL,
-	attribute_position NUMBER(2) NOT NULL,
-	question_about_api VARCHAR2(50) NOT NULL,
-	question_context_api VARCHAR2(50) NOT NULL,
+	attribute_position NUMBER NOT NULL,
+	question_about VARCHAR2(50) NOT NULL,
     	CONSTRAINT question_attributes_pk PRIMARY KEY (id),
-        CONSTRAINT question_unique UNIQUE (question_content_id, question_about_api, question_context_api),
+        CONSTRAINT question_unique UNIQUE (question_content_id, question_attribute, question_about),
         CONSTRAINT content_in_attributes_fk FOREIGN KEY (question_content_id) REFERENCES question_content(id) ON DELETE CASCADE
 );
 
 CREATE TABLE active_question (
 	token VARCHAR2(100) NOT NULL,
-    question_attributes_id NUMBER(10) NOT NULL,
+    question_attributes_id NUMBER NOT NULL,
     answer VARCHAR2(1000) NOT NULL,
     creation_date TIMESTAMP NOT NULL,
     	CONSTRAINT active_question_pk PRIMARY KEY (token),
@@ -34,10 +33,10 @@ CREATE TABLE active_question (
 );
 
 CREATE TABLE archives (
-	id NUMBER(10) NOT NULL,
-	question_attributes_id NUMBER(10) NOT NULL,
-    good_answers NUMBER(10) DEFAULT 0 NOT NULL,
-    bad_answers NUMBER(10) DEFAULT 0 NOT NULL,
+	id NUMBER NOT NULL,
+	question_attributes_id NUMBER NOT NULL,
+    good_answers NUMBER DEFAULT 0 NOT NULL,
+    bad_answers NUMBER DEFAULT 0 NOT NULL,
 		CONSTRAINT archives_pk PRIMARY KEY (id),
     	CONSTRAINT question_in_archives_unique UNIQUE (question_attributes_id),
     	CONSTRAINT question_in_archives_fk FOREIGN KEY (question_attributes_id) REFERENCES question_attributes(id) ON DELETE CASCADE
